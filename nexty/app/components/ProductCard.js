2baf1ddd38e96df770c27500c1216e69ea9514f8
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-09-13 10:47:27 +0200

    Add prevImage function to navigate to the previous image

--- nexty/app/components/ProductCard.js
+++ nexty/app/components/ProductCard.js
@@ -5,13 +5,16 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function ProductCard({ product }) {
-    const [currentImageIndex, setCurrentImageIndex] = useState(0);
-    const nextImage = () => {
-        setCurrentImageIndex(
-          (prevIndex) => (prevIndex + 1) % product.images.length
-        );
-      };
-      
-
-}  
-  
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const nextImage = () => {
+    setCurrentImageIndex(
+      (prevIndex) => (prevIndex + 1) % product.images.length
+    );
+  };
+  const prevImage = () => {
+    setCurrentImageIndex(
+      (prevIndex) =>
+        (prevIndex - 1 + product.images.length) % product.images.length
+    );
+  };
+}
