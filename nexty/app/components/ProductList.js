61dc860eb3d937395da526ae234252e3fd3ddee5
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-09-13 10:29:23 +0200

    Add imports for Image and Link components from Next.js

--- nexty/app/components/ProductList.js
+++ nexty/app/components/ProductList.js
@@ -1,10 +1,13 @@
+import Image from "next/image";
+import Link from "next/link";
+
+
 export default function ProductList({ product }) {
-    return (
-      <div className="border p-4 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
-        <h2 className="text-lg font-bold mb-2">{product.title}</h2>
-        <p className="text-gray-600 mb-4">{product.description}</p>
-        <p className="text-blue-500 font-semibold">${product.price}</p>
-      </div>
-    );
-  }
-  
+  return (
+    <div className="border p-4 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
+      <h2 className="text-lg font-bold mb-2">{product.title}</h2>
+      <p className="text-gray-600 mb-4">{product.description}</p>
+      <p className="text-blue-500 font-semibold">${product.price}</p>
+    </div>
+  );
+}
