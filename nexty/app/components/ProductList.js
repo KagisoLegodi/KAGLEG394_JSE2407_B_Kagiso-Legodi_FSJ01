fa8426dc8517d2fd617643ed04c81ffe326ff9ac
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-09-13 10:35:07 +0200

    Display product category

--- nexty/app/components/ProductList.js
+++ nexty/app/components/ProductList.js
@@ -16,7 +16,7 @@ export default function ProductList({ product }) {
       />
       <div className="border p-4 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
         <h2 className="text-lg font-bold mb-2">{product.title}</h2>
-        <p className="text-gray-600 mb-4">{product.description}</p>
+        <p className="text-gray-700">{product.category}</p>
         <p className="text-blue-500 font-semibold">${product.price}</p>
       </div>
     </Link>
