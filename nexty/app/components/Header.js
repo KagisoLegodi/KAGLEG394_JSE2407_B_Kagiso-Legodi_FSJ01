cc89c72b9a4b67231d17178fc1fd41b9095b92dc
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-09-13 11:03:32 +0200

    Add home link with site title

--- nexty/app/components/Header.js
+++ nexty/app/components/Header.js
@@ -2,11 +2,16 @@ import Link from "next/link";
 import { FaHeart, FaShoppingCart, FaUser } from "react-icons/fa";
 
 export default function Header() {
-    return (
-      <header className="bg-gray-600 p-4 shadow-lg">
-        <nav className="container mx-auto flex justify-between items-center">
-        </nav>
-      </header>
-    );
-  }
-  
+  return (
+    <header className="bg-gray-600 p-4 shadow-lg">
+      <nav className="container mx-auto flex justify-between items-center">
+        <Link
+          href="/"
+          className="text-2xl font-bold text-white hover:text-blue-200 transition duration-300"
+        >
+          Nexty E-Commerce
+        </Link>
+      </nav>
+    </header>
+  );
+}
