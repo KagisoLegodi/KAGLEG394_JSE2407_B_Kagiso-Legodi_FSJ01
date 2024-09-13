2c5c8581a5b707308764bd868b764d91da5857a8
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-09-13 14:44:19 +0200

    "Updated remotePatterns in next.config.mjs to include port and pathname properties"

--- nexty/next.config.mjs
+++ nexty/next.config.mjs
@@ -1,16 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "next-ecommerce-api.vercel.app",
-      },
-      {
-        protocol: "https",
-        hostname: "cdn.dummyjson.com",
-      },
-    ],
+      remotePatterns: [
+          {
+              protocol: 'https',
+              hostname: 'next-ecommerce-api.vercel.app',
+              port: '',
+              pathname: '/**',
+          },
+          {
+              protocol: 'https',
+              hostname: 'cdn.dummyjson.com',
+              port: '',
+              pathname: '/**',
+          },
+      ],
   },
 };
 
