976d014c9b307bbe7355a40126785a53b298d500
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-09-13 11:02:20 +0200

    Create basic Header component with container navigation

--- nexty/app/components/Header.js
+++ nexty/app/components/Header.js
@@ -1,2 +1,12 @@
 import Link from "next/link";
 import { FaHeart, FaShoppingCart, FaUser } from "react-icons/fa";
+
+export default function Header() {
+    return (
+      <header className="bg-gray-600 p-4 shadow-lg">
+        <nav className="container mx-auto flex justify-between items-center">
+        </nav>
+      </header>
+    );
+  }
+  
