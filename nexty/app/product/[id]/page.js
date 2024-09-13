e37bb0e36c538363f10d126be032b5f24b969924
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-09-13 11:28:26 +0200

    Add imports and getProduct function for fetching product data

---
+++ nexty/app/product/[id]/page.js
@@ -0,0 +1,17 @@
+"use client";
+import { useState, useEffect } from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+async function getProduct(productId) {
+  const res = await fetch(
+    `https://next-ecommerce-api.vercel.app/products/${productId}`,
+    { cache: "no-store" }
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch product");
+  }
+
+  return res.json();
+}
