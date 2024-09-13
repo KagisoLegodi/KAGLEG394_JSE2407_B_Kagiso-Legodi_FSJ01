13af12da59bcd6a0bf814583132871489d9916ac
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-09-13 10:57:17 +0200

    removed unnecessary comments

--- nexty/next.config.mjs
+++ nexty/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['next-ecommerce-api.vercel.app','cdn.dummyjson.com'], // Add your image domain here
+        domains: ['next-ecommerce-api.vercel.app','cdn.dummyjson.com'],
       },
 };
 
